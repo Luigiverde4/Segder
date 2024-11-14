de736328da78b7ac46fb1abb74aca4ba45c53b9d
R.Roman <luigiverdeyt@gmail.com>
2024-11-14 18:12:47 +0100

    Transferir archivos es posible

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,6 +1,26 @@
 from socket import *
 import os
-# Datos conexio
+
+def descarga(mensaje_tx: str)->None:
+    long = int(mensaje_rx[23:])
+    if mensaje_rx.decode().startswith("200"):
+        print("--------> Fichero recibido")
+        with open(f"contenido_descargado/{mensaje_tx.split()[1]}", "wb") as archivo:
+            cont_down = 0
+            while cont_down < long:
+                data = s.recv(2048)
+                cont_down += len(data)
+                archivo.write(data)
+    else:
+        print("Respuesta:", mensaje_rx)
+        print("--------> Fichero no encontrado")
+
+def ver():
+    # Creamos una lista partiendo cada \n
+    lista_contenidos = mensaje_rx.decode().split("\n")
+    print(lista_contenidos)  
+
+# Datos conexion
 dir_IP_servidor= '127.0.0.1'
 puerto_servidor = 6000
 dir_socket_servidor =(dir_IP_servidor, puerto_servidor)
@@ -23,13 +43,11 @@ max_len = max(len(comando) for comando in comandos) # Esto es para el formateo m
 print("Introduzca INFO para obtener información sobre los comandos que puedes enviar")
 
 while True:
-    mensaje_tx = input("\nIntroduzca su comando : ").upper()
-    
+    mensaje_tx = input("\nIntroduzca su comando : ")
 
     # Lidiar con el envio
-
     # Mostrar los comandos disponibles alineados
-    if mensaje_tx == "INFO":
+    if mensaje_tx.startswith("INFO"):
         print("\nComandos disponibles:")
         for instruccion in comandos:
             print(f"{instruccion.ljust(max_len)} - {comandos[instruccion]}")
@@ -38,17 +56,19 @@ while True:
     elif mensaje_tx.split()[0] not in [comando.split()[0] for comando in comandos]: 
         # Si el comando no existe, se avisa
         print("Comando erroneo")
+        continue
 
     # Terminar el programa
-    elif mensaje_tx == "FIN":
+    elif mensaje_tx.startswith("FIN"):
         s.send(mensaje_tx.encode())  # Enviar mensaje de cierre al servidor
         s.close() 
-        break  
+        continue  
 
     # Limpiar la consola 
-    elif mensaje_tx == "CLS":
+    elif mensaje_tx.startswith("CLS"):
         os.system("cls")
         print("Introduzca INFO para obtener información sobre los comandos que puedes enviar")
+        continue
 
     # Enviar mensaje
     else:
@@ -56,11 +76,9 @@ while True:
     
 
     # Lidiar con la respuesta
-    mensaje_rx = s.recv(2048).decode()  # Recibir
-    if mensaje_tx == "VER":
-        # Creamos una lista partiendo cada \n y la printeamos
-        lista_contenidos = mensaje_rx.split("\n")
-        print(lista_contenidos)    
-    elif mensaje_tx == "DESCARGAR":
-        # TODO hacer que descargue y cree el archivo al descargarlo
-        pass
+    mensaje_rx = s.recv(2048)  # Recibir
+    if mensaje_tx.startswith("VER"):
+        ver()
+
+    elif mensaje_tx.startswith("DESCARGAR"):
+        descarga(mensaje_tx)
--- ServerContenidos.py
+++ ServerContenidos.py
@@ -15,6 +15,42 @@ def log(msj: str)-> None:
     """
     print(f"{datetime.now().strftime('%H:%M:%S')} - {msj}")
 
+def ver()->None:
+    """
+    Mira y envia si hay contenidos
+    Args:
+        None
+    Returns:
+        None
+    """
+    archivos = os.listdir("contenido")
+    if (len(archivos) != 0):
+        # Si hay contenido
+        archivos_str = "\n".join(archivos)
+    else:
+        archivos_str = "No hay contenido\n"
+    s1.send(archivos_str.encode())
+
+def get(mensaje_rx:str)->None:
+    """
+    Coge y manda un archivo
+    Args:
+        nombre (str): Nombre del archivo a mandar
+    Returns:
+        None
+    """
+    nombre = mensaje_rx.split()[1]
+    print(f"Nombre : {nombre}")
+    ruta = f"contenido/{nombre}" 
+
+    with open(f"contenido/{nombre}", 'rb') as archivo:
+        cont = archivo.read()
+        long = os.stat(ruta).st_size  # Obtener solo el tamaño del archivo
+        msg = "200 Longitud Contenido:" + str(long) + "\n"  # Convertir a cadena
+        s1.send(msg.encode())
+        s1.sendall(cont)
+
+
 # Datos conexion
 dir_IP_server = "127.0.0.1"
 puerto_server = 6000
@@ -30,9 +66,9 @@ log(f"Conexion aceptada a {addr[0]}")
 # Variables
 comandos = ["VER","DESCARGAR","FIN"] 
 
-try:
-    # Bucle infinito que corre el servidor
-    while True:
+# Bucle infinito que corre el servidor
+while True:
+    try:
         # Recibir peticion
         mensaje_rx= s1.recv(2048).decode()
         log(f"Mensaje RX {mensaje_rx}")
@@ -41,25 +77,32 @@ try:
         if mensaje_rx.split()[0] not in [comando.split()[0] for comando in comandos]: # Si no se hace asi, descargar no va
             s1.send("Comando no reconocido".encode())
         
-        # Terminar el programa
-        elif (mensaje_rx == "FIN"):# Fin de la comunicacion con el cliente
-            s.close()
-            break
-        
         # Manejar el comando
         else:
-            if mensaje_rx == "VER":
-                archivos = os.listdir("contenido")
-                archivos_str = "\n".join(archivos)
-                s1.send(archivos_str.encode())
+            # FIN del programa
+            if mensaje_rx.startswith("FIN"):
+                s.close()
+                break
 
-except Exception as e:
-    # Capturar errores especificos y enviar un mensaje de error
-    log(f"Error en el servidor: {str(e)}")
-    s1.send("ERROR EN EL SERVIDOR".encode())
+            # VER los contenidos disponibles
+            elif mensaje_rx.startswith("VER"):
+                ver()
+            
+            # DESCARGAR contenido pedido
+            elif mensaje_rx.startswith("DESCARGAR"):
+                get(mensaje_rx)
+                #TODO Al encriptar, separar cabecera y cuerpo
+                        
+    except Exception as e:
+        # Capturar errores especificos y enviar un mensaje de error
+        log(f"Error en el servidor: {str(e)}")
+        s1.send("ERROR EN EL SERVIDOR".encode())
 
-# Cerrar el socket
-finally:
-    if s1:
-        s1.close()
-    log(f"Servidor cerrado")
+    except FileNotFoundError:
+        log(f"ERROR: Archivo no encontrado!")
+        s1.send(b"ERROR: Archivo no encontrado") 
+    # # Cerrar el socket
+    # finally:
+    #     if s1:
+    #         s1.close()
+    #     log(f"Servidor cerrado")
