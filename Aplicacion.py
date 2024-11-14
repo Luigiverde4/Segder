284f081e47e0ff10d201fb76acc091a525d1c569
R.Roman <luigiverdeyt@gmail.com>
2024-11-14 10:57:43 +0100

    Mejora sistema de comandos
    He mejorado el sistema para que  podamos meter mas comandos a futuro si queremos.
    
    Changelog:
    1. Creado un diccionario de instrucciones para poder escañar a futuro
    2. Añadido comentarios
    3. Formateado los prints de los comandos para que se lean mejorado
    4. Mejorado el input para capitalizar las entradas

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,27 +1,41 @@
 from socket import *
+
+# Datos conexio
 dir_IP_servidor= '127.0.0.1'
 puerto_servidor = 6000
 dir_socket_servidor =(dir_IP_servidor, puerto_servidor)
 
+# Socket
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(dir_socket_servidor)
+
+# Variables globales
+instrucciones = {
+    "VER": "Sirve para obtener los contenidos disponibles en el servidor",
+    "DESCARGAR {nombre del archivo}": "Envía una solicitud de descarga del fichero especificado al servidor",
+    "FIN": "Cierra la aplicación"
+}
+max_len = max(len(comando) for comando in instrucciones) # Esto es para el formateo mas tarde
+
+
 print("Introduzca INFO para obtener información sobre los comandos que puedes enviar")
+
 while True:
-    mensaje_tx = input("Introduzca su mensaje : ")
+    mensaje_tx = input("\nIntroduzca su comando : ").upper()
     if(mensaje_tx == "INFO"):
-        print("Comandos disponibles:")
-        print("VER - Sirve para obtener los contenidos disponibles en el servidor")
-        print("DESCARGAR {nombre del archivo} - Envia una solicitud de descarga del fichero especificado al servidor")
-        print("VER - Sirve para obtener los contenidos disponibles en el servidor")
-        print("FIN - Cierra la aplicación")
+        # Mostrar comandos con formato alineado
+        print("\nComandos disponibles:")
+        for instruccion in instrucciones:
+            print(f"{instruccion.ljust(max_len)} - {instrucciones[instruccion]}")
     elif(mensaje_tx == "FIN"):
+        # Cerrar socket
         s.send(mensaje_tx.encode())
         s.close()
-        break
+        break # return si cambiamos a funcion a futuro
+    elif (mensaje_tx not in instrucciones):
+        print("Comando erroneo")
     else:
+        # Enviar y Recibir mensajes
         s.send(mensaje_tx.encode())
         mensaje_rx = s.recv(2048)
         print(mensaje_rx.decode())
-
-
-
