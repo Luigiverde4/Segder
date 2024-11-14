a125f2dff292b1e2e08523d9cab54d9098b29993
Gabriel-Page-Chiva <gabriel.page.2003@gmail.com>
2024-11-14 21:39:06 +0100

    he programado el archivo de licencias para decir si existe el archivo o no y que diga si es encriptable, cuando hagamos mas cosas usaremos si es encriptable o no maybe

--- ServerLicencias.py
+++ ServerLicencias.py
@@ -1,3 +1,50 @@
-"""
-Servidor Licencias
-"""
+import json
+import os
+
+# Ruta al archivo JSON que contiene la información
+ruta_json = 'licencias.json'
+
+# Ruta a la carpeta 'contenido' donde están guardados los archivos
+ruta_contenido = 'contenido'
+
+def leer_json(ruta):
+    """
+    Servidor Licencias
+    
+    Abre el archivo JSON, lo lee y lo carga en un diccionario
+    
+    Parametros:
+    ruta (str): La ruta del archivo
+    
+    Retorna:
+    dict: El contenido del archivo JSON como un diccionario
+    """
+    with open(ruta, 'r') as file:
+        return json.load(file)
+
+def verificar_archivos(json_data, carpeta):
+    """
+    Servidor Licencias
+    
+    Recorre el archivo JSON y comprueba que exista el archivo y te dice si es es encriptable
+    #cuando tengamos mas cosas hechas podemos enlazarlo bien
+    
+    Parametros:
+    json_data (dict): El diccionario que contiene la información de los archivos
+    carpeta (str): La ruta de la carpeta donde se encuentran los archivos
+    """
+    archivos_en_carpeta = os.listdir(carpeta)
+    
+    # Recorre cada archivo en el JSON y verifica si existe en la carpeta
+    for archivo_info in json_data['archivos']:
+        archivo_nombre = archivo_info['nombre']
+        encriptable = archivo_info['encriptable']
+        
+        # Verifica si el archivo esta presente en la carpeta 'contenido'
+        if archivo_nombre in archivos_en_carpeta:
+            print(f"El archivo '{archivo_nombre}' esta en la carpeta. Encriptable: {encriptable}")
+        else:
+            print(f"El archivo '{archivo_nombre}'no se encuentra en la carpeta. Encriptable: {encriptable}")
+
+datos_json = leer_json(ruta_json)
+verificar_archivos(datos_json, ruta_contenido)
